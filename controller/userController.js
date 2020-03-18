8428afe92685ad654485c89a9a1b4dad4ec1d376
wangxiping@banggood.com <wangxiping@banggood.com>
2020-03-18 19:55:03 +0800

    查询菜单

--- controller/userController.js
+++ controller/userController.js
@@ -15,11 +15,9 @@ class UserController {
 
     try {
       let _data = await UserServer.login(phone)
+      let menu = await UserServer.getMenu(_data.user_id)
       if (!_data) throw 'USER_NOT_EXITS'
-      // if (_data.phone !== phone) {
-      //   res.R.err('USER_PASSWORD_WRONG')
-      //   return false
-      // }
+     
       if (_data.password !== password) {       
         res.R.err('USER_PASSWORD_WRONG')
         return false
@@ -30,10 +28,29 @@ class UserController {
         nickName: _data.nick_name,
         admin: true,
       }
+      //  遍历菜单
+      function menuTree(data) {
+        let arr = [] // 存储一级菜单
+        function tree(data) {
+          data.map((it, idx) => {
+            if (it.parent_id) {
+              arr.map((i, d) => {
+                if (i.res_id === it.parent_id) {
+                  i.children = it
+                }
+              })
+            } else {
+              arr.push(it)
+            }
+          })
+        }
+        tree(data)
+        return arr
+      }
       let token = JwtToken.createToken(payload) // 签发
       redis.set(nickName, token, JWT_COMF.JWTEXP) //  同步到redis
       res.cookie('nickName', nickName, { maxAge: 900000, httpOnly: true }) // 设置cookie
-      res.R.ok({token:token})
+      res.R.ok({token:token, menuList: menuTree(menu) })
     } catch (ex) {
       res.R.err(ex)
     }
@@ -41,6 +58,9 @@ class UserController {
   // 退出
   loginOut(req, res) {
 
+  }
+  getMeunList (req, res) {
+    
   }
   // 获取用户信息
   getUserInfo(req, res) {
--- server/userServer.js
+++ server/userServer.js
@@ -13,7 +13,13 @@ const { exec } = require ('../db/mysql.js')
       }
     }
     async testSql () {
-      let sql = 'select sys_resource.*  from sys_user_role, sys_role_permmision, sys_resource  where user_id = 1 group by `res_id`'
+      let sql = 'select sys_resource.*  from sys_user_role, sys_role_permmision, sys_resource  where user_id = 1 group by res_id'
+      let row = await exec(sql)
+      return row
+    }
+
+    async getMenu (userId) {
+      let sql = `select sys_resource.*  from sys_user_role, sys_role_permmision, sys_resource  where user_id =${userId} group by res_id`
       let row = await exec(sql)
       return row
     }
