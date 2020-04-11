f338f43f2ba99c659291b98f890df37716c4c2f0
wangxiping <395604192@qq.com>
2020-04-11 20:53:40 +0800

    修改配置

--- conf/db.js
+++ conf/db.js
@@ -1,24 +1,39 @@
-//mysql配置文件
-// mysqlConf = { // mac 本地
-//   host: "127.0.0.1", //数据库的地址
-//   user: "root", // 用户的名字
-//   password: "csdewang", // 用户密码 ，
-//   database: "blog" //数据库名字
-// }
-mysqlConf = { // 服务器
-  host: "106.54.63.174", //数据库的地址
-  user: "root", // 用户的名字
-  password: "wang", // 用户密码 ，
-  database: "blog", //数据库名字,
-  useConnectionPooling: true
-} 
-REDIS_CONF = {
-  port: 6379,
-  host: '127.0.0.1'
+const env = process.env.NODE_ENV  // 环境参数
+console.log(env)
+if (env === 'dev') {
+  mysqlConf = { // 服务器
+    host: "106.54.63.174", //数据库的地址
+    user: "root", // 用户的名字
+    password: "wang", // 用户密码 ，
+    database: "blog", //数据库名字,
+    useConnectionPooling: true
+  } 
+  REDIS_CONF = {
+    port: 6379,
+    host: '127.0.0.1'
+  }
+  JWT_COMF = {
+    JWTKEY: 'wangxiping',
+    JWTEXP: 3600 // token 过期时间
+  }
 }
-JWT_COMF = {
-  JWTKEY: 'wangxiping',
-  JWTEXP: 3600 // token 过期时间
+
+if (env === 'production') {
+  mysqlConf = { // 服务器
+    host: "106.54.63.174", //数据库的地址
+    user: "root", // 用户的名字
+    password: "wang", // 用户密码 ，
+    database: "blog", //数据库名字,
+    useConnectionPooling: true
+  } 
+  REDIS_CONF = {
+    port: 6379,
+    host: '127.0.0.1'
+  }
+  JWT_COMF = {
+    JWTKEY: 'wangxiping',
+    JWTEXP: 3600 // token 过期时间
+  }
 }
 module.exports = {
   mysqlConf,
