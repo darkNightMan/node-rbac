20252c9af0d8a24bd664d569aae3cbb8c6cfa874
wangxiping@banggood.com <wangxiping@banggood.com>
2020-04-14 18:17:12 +0800

    更新

---
+++ server/SysLogServer.js
@@ -0,0 +1,18 @@
+const {
+  exec
+} = require('../db/mysql.js')
+
+// 登入1
+class SysLogServer {
+  insert (data) {
+    let sql = `INSERT INTO sys_loging_log(user_log_id, user_name, login_time, login_ip, login_address, user_role) VALUES(
+      ${data.user_id},
+      ${data.user_id}
+      ${data.user_id}
+      ${data.user_id}
+      ${data.user_id}
+    )`
+  }
+}
+
+module.exports = new SysLogServer()
