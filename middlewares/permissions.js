5aea63fdfc0ef30970163ab09005224e19d04117
wangxiping <395604192@qq.com>
2020-04-24 23:32:09 +0800

    添加权限

---
+++ middlewares/permissions.js
@@ -0,0 +1,12 @@
+// const perms = (identification) => {
+//   return async function (req, res, next) {
+//     console.log('Identification',Identification)
+//     next()
+//   }
+// }
+
+// module.exports = perms
+
+class Permissions {
+  
+}
