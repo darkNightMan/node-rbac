c3b62924f4ded4ad03353f7ff66d5d9fb140bb37
wangxiping <395604192@qq.com>
2020-04-25 20:07:19 +0800

    修改权限

--- middlewares/permissions.js
+++ middlewares/permissions.js
@@ -5,7 +5,7 @@ class Permissions {
     return async function (req, res, next) {
       let userid = req.userInfo.user_id
       let perms = await redis.get(`user_perms_${userid}`)
-      let hasPows = perms.split(',').every(it => it === authName)
+      let hasPows = perms.split(',').some(it => it === authName)
       if (!hasPows) {
         return res.R.err('USER_PERMS_EXITS')
       }
--- router/index.js
+++ router/index.js
@@ -17,24 +17,24 @@ module.exports = function (app) {
   app.get('/api/loginOut', UserController.loginOut) // 退出 
   app.get('/api/getUserMenuList', UserController.getUserMenuList) // 获取用户信息和菜单
  // 角色
-  app.get('/api/role/getRoleTreePer/', SysRoleController.getRoleTreePer) // 获取当前角色下的权限
-  app.post('/api/role/setRoleTreePer/', SysRoleController.setRoleTreePer) //修改角色权限
-  app.get('/api/role/list',  SysRoleController.list) // 获取所有的角色  permissions.hasPerms('sys:menu:list'),
-  app.post('/api/role/createRole', permissions.hasPerms('sys:menu:create'), SysRoleController.createRole) // 新建角色 
-  app.put('/api/role/updateRole', SysRoleController.updateRole) // 更新角色  permissions.hasPerms('sys:menu:update'), 
-  app.delete('/api/role/deleteRole', permissions.hasPerms('sys:menu:delete'), SysRoleController.deleteRole)  // 删除角色 
+  app.get('/api/role/getRoleTreePer/',permissions.hasPerms('sys:role:lookPerms'), SysRoleController.getRoleTreePer) // 获取当前角色下的权限 
+  app.post('/api/role/setRoleTreePer/', permissions.hasPerms('sys:role:editPerms'), SysRoleController.setRoleTreePer) // 修改角色权限
+  app.get('/api/role/list', permissions.hasPerms('sys:role:list'), SysRoleController.list) // 获取所有的角色  
+  app.post('/api/role/createRole', permissions.hasPerms('sys:role:create'), SysRoleController.createRole) // 新建角色 
+  app.put('/api/role/updateRole', permissions.hasPerms('sys:role:update'), SysRoleController.updateRole) // 更新角色名  
+  app.delete('/api/role/deleteRole', permissions.hasPerms('sys:role:delete'), SysRoleController.deleteRole)  // 删除角色 
 // 用户
-  app.get('/api/user/list', permissions.hasPerms('sys:menu:list'), SysUserController.list) // 获取所有的用户
-  app.post('/api/user/createUser', permissions.hasPerms('sys:menu:create'), SysUserController.createUser) // 新增用户
-  app.put('/api/user/updateUser', permissions.hasPerms('sys:menu:update'), SysUserController.updateUser) // 更新用户
-  app.delete('/api/user/deleteUser', permissions.hasPerms('sys:menu:delete'), SysUserController.deleteUser) // 删除用户
+  app.get('/api/user/list', permissions.hasPerms('sys:user:list'), SysUserController.list) // 获取所有的用户
+  app.post('/api/user/createUser', permissions.hasPerms('sys:user:create'), SysUserController.createUser) // 新增用户
+  app.put('/api/user/updateUser', permissions.hasPerms('sys:user:update'), SysUserController.updateUser) // 更新用户
+  app.delete('/api/user/deleteUser', permissions.hasPerms('sys:user:delete'), SysUserController.deleteUser) // 删除用户
 // 菜单
-  app.get('/api/menu/list/', SysMenuController.list) // 获取菜单列表   permissions.hasPerms('sys:menu:list'),
   app.get('/api/menu/selectMenuList', SysMenuController.selectMenuList)
   app.get('/api/menu/sysMenutree/', SysMenuController.treeMenu) // 获取菜单树
-  app.post('/api/menu/createMenu',  SysMenuController.createMenu) // 新增菜单 permissions.hasPerms('sys:menu:create'),
-  app.put('/api/menu/updatedMenu',  SysMenuController.updatedMenu) // 更新菜单 permissions.hasPerms('sys:menu:update'),
-  app.delete('/api/menu/deleteMenu', SysMenuController.deleteMenu) // 删除菜单  permissions.hasPerms('sys:menu:delete'), 
+  app.get('/api/menu/list/', permissions.hasPerms('sys:menu:list'), SysMenuController.list) // 获取菜单列表  
+  app.post('/api/menu/createMenu', permissions.hasPerms('sys:menu:create'), SysMenuController.createMenu) // 新增菜单 
+  app.put('/api/menu/updatedMenu', permissions.hasPerms('sys:menu:update'), SysMenuController.updatedMenu) // 更新菜单 
+  app.delete('/api/menu/deleteMenu', permissions.hasPerms('sys:menu:delete'), SysMenuController.deleteMenu) // 删除菜单  
 
   app.get('/api/loginLogs/list', permissions.hasPerms('sys:logs:list'), SysLoginLogsController.list) // logs列表  
 
